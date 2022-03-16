73ccfd5c9ab3084ab0f9dbad51732f7a397d0d8a
EC2 Default User <ec2-user@ip-172-31-39-113.ec2.internal>
2022-03-16 05:53:16 +0000

    add admin customer db

--- db/migrate/20220316050308_devise_create_customers.rb
+++ db/migrate/20220316050308_devise_create_customers.rb
@@ -13,6 +13,15 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
 
       ## Rememberable
       t.datetime :remember_created_at
+      
+      t.string :last_name
+      t.string :first_name
+      t.string :last_name_kana
+      t.string :first_name_kana
+      t.string :postal_code
+      t.string :address
+      t.string :telephone_number
+      t.boolean :is_deleted, default: "false"
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
