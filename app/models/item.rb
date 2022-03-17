cad8d702c0aae8e97a651edf9ac2fb42cab581cf
EC2 Default User <ec2-user@ip-172-31-13-209.ec2.internal>
2022-03-17 07:45:48 +0000

    Add item_new,index

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+
+  has_many :order_details
+  has_many :orders, through: :order_details
+  has_many :order_details
+  belongs_to :genre
+
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :image, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
+
 end
