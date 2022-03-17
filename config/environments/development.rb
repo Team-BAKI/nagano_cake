cc855cc780bdd2286d3a58f524cfa22c90e749ce
EC2 Default User <ec2-user@ip-172-31-39-113.ec2.internal>
2022-03-17 06:04:23 +0000

    mergetest

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "6f2139a9c44442549bdd56a3bdcd1fb6.vfs.cloud9.us-east-1.amazonaws.com"
+  config.hosts << "5e4ac7702a4040f0a1ec9c166a95e526.vfs.cloud9.us-east-1.amazonaws.com"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
     resources :items, only: [:index, :show]
 
-    resources :cart_items, only: [:index, :update, :destroy, :create] 
+    resources :cart_items, only: [:index, :update, :destroy, :create]
 
     delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
 
@@ -31,7 +31,7 @@ Rails.application.routes.draw do
     get '/orders/complete' => 'orders#complete'
 
   end
- 
+
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
   }
