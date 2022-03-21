e108c137003797b5f4011ef4972134d592863f36
Kohei Mihara <mhr.k135@gmail.com>
2022-03-21 06:15:42 +0000

    revise_admin_test

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -5,6 +5,7 @@ class Admin::OrderDetailsController < ApplicationController
     @order_detail.update(order_detail_params)
     @order = @order_detail.order
     if @order.order_details.all?{|o| o.making_status_before_type_cast == 3}
+      # 製作ステータスが全て製作完了→注文ステータスが発送準備中
       @order.update(status: 3)
     elsif @order.order_details.where(making_status: 2..3).exists?
       @order.update(status: 2)
