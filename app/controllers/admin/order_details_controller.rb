836a4e48438a5f8406e7a270ffab3017178e1d39
Kohei Mihara <mhr.k135@gmail.com>
2022-03-21 03:30:51 +0000

    test

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,6 +3,12 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
     @order_detail.update(order_detail_params)
+    @order = @order_detail.order
+    if @order.order_details.all?{|o| o.making_status_before_type_cast == 3}
+      @order.update(status: 3)
+    elsif @order.order_details.where(making_status: 2..3).exists?
+      @order.update(status: 2)
+    end
     redirect_to admin_order_path(@order_detail.order)
   end
 
