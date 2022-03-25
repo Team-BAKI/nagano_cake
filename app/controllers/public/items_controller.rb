fb7bd5cec404c6fa685e6c811b78187c4f2ccf72
Kohei Mihara <mhr.k135@gmail.com>
2022-03-25 01:14:42 +0000

    revise status

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,7 @@
 class Public::ItemsController < ApplicationController
   def index
     @genres = Genre.all
-    @items = Item.all.page(params[:page]).per(8)
+    @items = Item.where(is_active: 1).page(params[:page]).per(8)
   end
 
   def show
@@ -13,7 +13,7 @@ class Public::ItemsController < ApplicationController
   def search
     @genres = Genre.all
     @genre = Genre.find(params[:item_id])
-    @items = Item.where(genre_id: params[:item_id]).page(params[:page]).per(8)
+    @items = Item.where(genre_id: params[:item_id]).where(is_active: 1).page(params[:page]).per(8)
   end
 
 end
